2be53c6d9609519a2c002dea57019ac00fd02c7c
tomaspani <tomas.paniego@davinci.edu.ar>
2023-05-13 18:28:23 -0300

    camera dinamica

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,16 +8,18 @@ public class CameraController : MonoBehaviour
 
     [Range(1, 5)] [SerializeField] float height;
 
+    float _startingPlayerHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _startingPlayerHeight = target.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, height, target.transform.position.z);
+        transform.position = new Vector3(target.transform.position.x, height + (target.transform.position.y - _startingPlayerHeight), target.transform.position.z);
 
 
     }
